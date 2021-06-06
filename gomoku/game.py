9d0fa7eaecc2ae5c562da9c8bf1fb726fb4c2508
AloySobek <supernova.wandering.star@gmail.com>
2021-06-07 00:48:36 +0300

    Player_move method added in Game class next_move replace with ai_move

--- gomoku/game.py
+++ gomoku/game.py
@@ -1,3 +1,8 @@
+from gomoku.ai import Ai
+from gomoku.vec2 import Vec2
+from gomoku.board import Board
+from gomoku.board_analyzer import BoardAnalyzer
+
 import sys
 
 class Game:
@@ -5,17 +10,26 @@ class Game:
     controller = None
 
     def __init__(self):
-        self.board = [[0 for i in range(Game.board_size)] for j in range(Game.board_size)]
+        self.ai = Ai()
+        self.board = Board(Game.board_size)
+        self.board_analyzer = BoardAnalyzer(Game.board_size)
 
-    def next_move(self):
-        for y in range(Game.board_size):
-            for x in range(Game.board_size):
-                if self.board[y][x] == 0:
-                    self.board[y][x] = 2
-                    return x, y
+    def ai_move(self):
+        move = None
+        max_h = float("-inf")
+        for i in list(self.board_analyzer.possible_moves.keys()):
+            if self.board.at(i) == 0:
+                self.board.place_stone(i, True)
+                self.board_analyzer.add_surroundings(self.board, i)
+                h = self.ai.minimax(3, float("-inf"), float("inf"), self.board, self.board_analyzer, True)
+                if h >= max_h:
+                    max_h = h
+                    move = i
+                self.board_analyzer.remove_surroundings(self.board, i)
+                self.board.remove_stone(i, True)
+        self.board.place_stone(move, True)
+        return move
 
-    def set(self, x, y, value):
-        self.board[y][x] = value
-
-    def is_valid_move(self, x, y, value):
-        return self.board[y][x] == 0
+    def player_move(self, point: Vec2):
+        self.board.place_stone(point, False)
+        self.board_analyzer.add_surroundings(self.board, point)

698dd05622926d1bab54ae941e36fd6e8db2e7f8
AloySobek <supernova.wandering.star@gmail.com>
2021-06-07 00:47:16 +0300

    Gomoku ai minimax with alpha beta pruning added

---
+++ gomoku/ai.py
@@ -0,0 +1,36 @@
+from gomoku.board import Board
+from gomoku.board_analyzer import BoardAnalyzer
+
+class Ai:
+    def __init__(self):
+        pass
+
+    def minimax(self, depth: int, alpha: int, beta: int, board: Board, board_analyzer: BoardAnalyzer, is_black: bool):
+        if depth == 0 or board_analyzer.five_in_a_row(board, is_black) != 0:
+            return board_analyzer.eval_board(board, is_black)
+        if is_black:
+            max_h = float("-inf")
+            for i in list(board_analyzer.possible_moves.keys()):
+                if board.at(i) == 0:
+                    board.place_stone(i, False)
+                    board_analyzer.add_surroundings(board, i)
+                    max_h = max(max_h, self.minimax(depth-1, alpha, beta, board, board_analyzer, False))
+                    if max_h >= beta:
+                        break
+                    alpha = max(alpha, max_h)
+                    board_analyzer.remove_surroundings(board, i)
+                    board.remove_stone(i, False)
+            return max_h
+        else:
+            min_h = float("inf")
+            for i in list(board_analyzer.possible_moves.keys()):
+                if board.at(i) == 0:
+                    board.place_stone(i, True)
+                    board_analyzer.add_surroundings(board, i)
+                    min_h = min(min_h, self.minimax(depth-1, alpha, beta, board, board_analyzer, True))
+                    if min_h <= alpha:
+                        break
+                    beta = min(beta, min_h)
+                    board_analyzer.remove_surroundings(board, i)
+                    board.remove_stone(i, True)
+            return min_h
